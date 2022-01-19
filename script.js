33b3bac61228b82471fe63a827ed4631c135489d
Winston McHetz <winstonmchetz@R2Hs-MBP.attlocal.net>
2022-01-19 16:59:25 -0500

    update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Challenge 1
+
 function min(num1, num2){
     if(num1 < num2) return num1;
     else
@@ -5,4 +7,11 @@ function min(num1, num2){
 }
 console.log(min(15,12));
 console.log(min(12,15));
+
+//Challenge 2
+
+const students = ["Tom Massey", 17, "John Carter", 18, "Elliot James", 19];
+let student = students[0];
+console.log(students[0]` is `, students[1] `years old`);
+//Challange 3
 
